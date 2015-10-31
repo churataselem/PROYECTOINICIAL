cb6c136732598f05b00f0c7e9a164bafb0dcc0d0
churataselem <selemcedec@gamil.com>
2015-10-31 09:49:17 -0400

    ejemplos de operador

---
+++ routes/ejemplo.4.js
@@ -0,0 +1,12 @@
+
+console.log ("suma de numeros 13 + 7 ;") ;//20
+console.log (13 + 7 );//20
+console.log ("resta de numeros  13 - 7 : "); //6
+console.log (13 - 7 ); //6
+console.log ("exprecion con parentesis (8 * 9 - 4 )/ 3:"); //4
+console.log ((8*2 -4 )/3);//4
+
+//console.log ("ejecucion se interumpe:"");//error_de_ejecucion
+//console-log(8 / * 3); // error_de_ejecucion
+//console.log ("ejecucion se interumpe :");//*error_de_ejecucion
+//console-log(8 3 ); // error_de_ejecucion
---
+++ routes/ejemplo5.js
@@ -0,0 +1,12 @@
+
+console.log ("suma de numeros 13 + 7 :") ;//20
+console.log (13 + 7 );//20
+console.log ("signo de un numero  +13 : "); //13
+console.log (+13 ); //6
+console.log ("concatenacion de strings  \"hola \" + \"pepe\" :" ); //4
+console.log ("hola" + "pepe"  );
+
+//console.log ("ejecucion se interumpe:"");//error_de_ejecucion
+//console-log(8 / * 3); // error_de_ejecucion
+//console.log ("ejecucion se interumpe :");//*error_de_ejecucion
+//console-log(8 3 ); // error_de_ejecucion
