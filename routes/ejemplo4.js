5d0eaac76bfa48107b51b5d48d726c0b1dd015fc
churataselem <selemcedec@gamil.com>
2015-10-31 09:52:24 -0400

    ejemplos de operador

--- routes/ejemplo4.js
+++ routes/ejemplo4.js
@@ -1,10 +1,12 @@
 
-console.log ("suma de numeros 13 + 7 :") ;//20
+console.log ("suma de numeros 13 + 7 :") ;
 console.log (13 + 7 );//20
-console.log ("resta de numeros  13 - 7 : "); //6
+console.log ("resta de numeros  13 - 7 : "); 
 console.log (13 - 7 ); //6
-console.log ("exprecion con parentesis (8 * 9 - 4 )/ 3:"); //4
-console.log ((8*2 -4 )/3);//4
+console.log ("exprecion con parentesis (8 * 9 - 4 )/ 3:"); 
+console.log ("13"+ "7");
+console.log ("13"+ (+7);
+console.log (+"13" +7);
 
 //console.log ("ejecucion se interumpe:"");//error_de_ejecucion
 //console-log(8 / * 3); // error_de_ejecucion
